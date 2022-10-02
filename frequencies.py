97045ed8ceb2d0f5c773b22352f392f9641e97b4
Chandru Suresh <k21012031@kcl.ac.uk>
2022-10-02 20:57:42 +0100

    Implement frequencies method

--- frequencies.py
+++ frequencies.py
@@ -3,5 +3,13 @@
 
 def frequencies(items):
     frequencies = {}
-    # Your code goes here
+    if items is None:
+        return frequencies
+
+    for item in items:
+        if item in frequencies:
+            frequencies[item] += 1
+        else:
+            frequencies[item] = 1
+
     return frequencies
